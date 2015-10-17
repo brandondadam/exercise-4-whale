af4208bf98e3a021fb1b97e98ecc1b78774430f4
bdadam <bdadam@mail.bradley.edu>
2015-10-17 16:26:34 -0500

    tried to finish the assignment in one go
    seems to be some typos in the assignment

--- whale.js
+++ whale.js
@@ -1,10 +1,18 @@
-jQuery(document).ready(function($){
-		var num = parseInt(location.hash.substr(1));
-		console.log(num);
-		$.get('/whale/txt/' + num + '.txt', function(txt) {
-				console.log(txt);
-		});
-		if (isNaN(num)) {
-			num = 0;
-		}
-});
+jQuery(document).ready(function($) {
+	update();
+	window.addEventListener('hashchange', update, false)
+} );
+
+function update() { //Updates the text
+	var num = parseInt(location.hash.substr(1));
+
+	if (isNaN(num))
+	{
+		num = 0;
+	}
+
+	$.get('/whale/txt/'+num+'.txt', function(txt) {
+		$('a').html(txt);
+		$('a').attr('href', '#'+(num+1));
+	});
+}
