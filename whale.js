d0af15da8a6a58098a6b7d33c08950e18f9f926c
bdadam <bdadam@mail.bradley.edu>
2015-10-17 16:10:46 -0500

    added ajax

--- whale.js
+++ whale.js
@@ -1,7 +1,10 @@
 jQuery(document).ready(function($){
 		var num = parseInt(location.hash.substr(1));
 		console.log(num);
-		if (isNaN(num)){
+		$.get('/whale/txt/'+num+'.txt', function(txt)){
+			consle.log(txt);
+		});
+		if (isNaN(num)) {
 			num = 0;
 		}
 });
