f9d9824e7f96e5fef6a1070e37bae5c8174d0fd8
bdadam <bdadam@mail.bradley.edu>
2015-10-17 16:13:02 -0500

    fixed ajax maybe

--- whale.js
+++ whale.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function($){
 		var num = parseInt(location.hash.substr(1));
 		console.log(num);
-		$.get('/whale/txt/'+num+'.txt', function(txt)){
-			consle.log(txt);
+		$.get('/whale/txt/' + num + '.txt', function(txt)) {
+				console.log(txt);
 		});
 		if (isNaN(num)) {
 			num = 0;
